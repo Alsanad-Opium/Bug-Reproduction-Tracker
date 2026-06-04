82b9beecb645b167441bcd8ec38627a0e2acbcec
Alsanad-Opium <alsanad112006@gmail.com>
2026-06-04 14:19:11 +0530

    Update the schema

--- backend/app/models/projects.py
+++ backend/app/models/projects.py
@@ -7,7 +7,7 @@ class Project(db.Model):
     __tablename__ ="project"
     
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100),nullable = False)
+    name = db.Column(db.String(100),nullable = False, unique = True)
     
     description = db.Column(db.Text, nullable = True)
     
