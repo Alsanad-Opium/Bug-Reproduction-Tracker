7018499116a95f7074d6e454141b8758dcd55f60
Alsanad-Opium <alsanad112006@gmail.com>
2026-06-07 15:57:49 +0530

    Update: Added authorizatio authentication

--- backend/app/routes/bug_routes.py
+++ backend/app/routes/bug_routes.py
@@ -1,13 +1,16 @@
 from flask import Blueprint, request, jsonify
 from app import db
 from app.models.bugs import Bug
+from app.models.projects import Project
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 bugs_bp = Blueprint('bugs',__name__,url_prefix = '/api/bugs' )
 
 @bugs_bp.route('', methods = ['GET'], strict_slashes = False)
-
+@jwt_required()
 def get_all_bugs():
-    bugs = Bug.query.all()
+    user_id =get_jwt_identity()
+    bugs = Bug.query.join(Project).filter(Project.owner_id == user_id).all()
     
     if not bugs :
         return jsonify({"message":"No bugs found"}),404 # if db is empty
@@ -16,6 +19,7 @@ def get_all_bugs():
 
 
 @bugs_bp.route('/<int:id>', methods = ['GET'], strict_slashes = False)
+@jwt_required()
 def get_bug(id):
     bug = db.session.get(Bug, id)
     if bug is None:
@@ -23,11 +27,19 @@ def get_bug(id):
     return jsonify(bug.to_dict()), 200
 
 @bugs_bp.route('',methods = ['POST'], strict_slashes = False)
+@jwt_required()
 def create_bug():
     data = request.get_json()
-    
+    user_id =get_jwt_identity()
     if not data or not data['name'] or not data['description'] or not data['owner_id']:
         return jsonify({"message":"Missing required fields"}),400
+    #check project ownership before creating a bug
+    project = db.session.get(Project, data['project_id'])
+    if not project:
+        return jsonify({"message": "Project not found"}), 404
+
+    if project.owner_id != user_id:
+        return jsonify({"message": "Unauthorized"}), 403
     
     bug = Bug(name = data['name'],
                       description = data['description'], 
@@ -40,7 +52,7 @@ def create_bug():
     return jsonify({'message': 'bug created succesfully', 'bug': bug.to_dict()}),201
 
 @bugs_bp.route('/<int:id>/status', methods = ['PUT'], strict_slashes = False)
-
+@jwt_required()
 def update_bug_status(id):
     bug = db.session.get(Bug,id)
     if bug is None:
@@ -58,7 +70,7 @@ def update_bug_status(id):
     return jsonify({'message': 'bug status updated sucessfully', 'bug': bug.to_dict()}),200
 
 @bugs_bp.route('/<int:id>/priority', methods = ['PUT'], strict_slashes = False)
-
+@jwt_required()
 def update_bug_priority(id):
     bug = db.session.get(Bug,id)
     if bug is None:
--- backend/app/routes/project_routes.py
+++ backend/app/routes/project_routes.py
@@ -1,12 +1,13 @@
 from flask import Blueprint, request, jsonify
 from app import db
 from app.models.project import Project
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 project_bp = Blueprint('project_bp', '__name__', url_prefix = '/api/project')
 
 
 @project_bp.route('/get_all_projects', methods = ['GET'], strict_slashes = False)
-
+@jwt_required()
 def get_all_projects():
     projects = Project.query.all()
     
@@ -18,6 +19,7 @@ def get_all_projects():
 
 
 @project_bp.route('/get_project/<int:id>', methods = ['GET'], strict_slashes = False)
+@jwt_required()
 def get_project(id):
     project = Project.query.get(id)
     if project is None:
@@ -26,15 +28,18 @@ def get_project(id):
 
 
 @project_bp.route('/create_project',methods = ['POST'], strict_slashes = False)
+@jwt_required()
+
 def create_project():
     data = request.get_json()
     
     if not data or not data['name'] or not data['description'] or not data['owner_id']:
         return jsonify({"message":"Missing required fields"}),400
     
+    user_id = get_jwt_identity()
     project = Project(name = data['name'],
                       description = data['description'], 
-                      owner_id = data['owner_id']
+                      owner_id = user_id
     )
     db.session.add(project)
     db.session.commit()
@@ -42,12 +47,16 @@ def create_project():
     return jsonify({'message': 'project created succesfully', 'project': project.to_dict()}),201
 
 @project_bp.route('/<int:id>',methods = ['PUT'], strict_slashes=False) 
+@jwt_required()
 def update_project(id):
-    
+    user_id = get_jwt_identity() 
     project = db.session.get(Project,id)
     
     if project is None:
         return jsonify({'message': 'project not found'}),404
+    
+    if project.owner_id != user_id:
+        return jsonify({"message": "Unauthorized"}), 403
     
     data = request.get_json()
     
